04f3c0f6236d205ff292113396747dcecb81df1d
Jamie Mansfield <dev@jamierocks.uk>
2016-09-15 18:50:32 +0100

    Improve the slightly more advanced calculator

--- examples/improved_calc.asm
+++ examples/improved_calc.asm
@@ -1,23 +1,26 @@
 START    INP
-         BRZ SUBTRACT
-         BRA ADDITION
+         STA MODE
 
-ADDITION INP
-         STA VALUE_O
          INP
-         ADD VALUE_O
-         BRA END
-
-SUBTRACT INP
          STA VALUE_O
          INP
          STA VALUE_T
-         LDA VALUE_O
+
+         LDA MODE
+         BRZ SUBTRACT
+         BRA ADDITION
+
+ADDITION LDA VALUE_O
+         ADD VALUE_T
+         BRA END
+
+SUBTRACT LDA VALUE_O
          SUB VALUE_T
          BRA END
 
 END      OUT
          HLT
 
+MODE     DAT 1
 VALUE_O  DAT 1
 VALUE_T  DAT 2
