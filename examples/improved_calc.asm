6412a4555296c78303e513ec6cea30f72b43bbbf
Jamie Mansfield <dev@jamierocks.uk>
2016-09-16 21:02:31 +0100

    Comment out improved_calc.asm

--- examples/improved_calc.asm
+++ examples/improved_calc.asm
@@ -1,26 +1,26 @@
-START    INP
-         STA MODE
+START    INP          # Waits for input and stores it in the accumulator
+         STA MODE     # Stores the contents of the accumulator into the memory location of the label: MODE
 
-         INP
-         STA VALUE_O
-         INP
-         STA VALUE_T
+         INP          # Waits for input and stores it in the accumulator
+         STA VALUE_O  # Stores the contents of the accumulator into the memory location of the label: VALUE_O
+         INP          # Waits for input and stores it in the accumulator
+         STA VALUE_T  # Stores the contents of the accumulator into the memory location of the label: VALUE_T
 
-         LDA MODE
-         BRZ SUBTRACT
-         BRA ADDITION
+         LDA MODE     # Loads the contents of the memory location of the label: MODE to the accumulator
+         BRZ ADDITION # Branches to the memory location of the label: ADDITION is the contents of the accumulator are 0
+         BRA SUBTRACT # Branches to the memory location of the label: SUBTRACT
 
-ADDITION LDA VALUE_O
-         ADD VALUE_T
-         BRA END
+ADDITION LDA VALUE_O  # Loads the contents of the memory location of the label: VALUE_O to the accumulator
+         ADD VALUE_T  # Add the contents of memory address of the label: NUM to the accumulator
+         BRA END      # Branches to the memory location of the label: END
 
-SUBTRACT LDA VALUE_O
-         SUB VALUE_T
-         BRA END
+SUBTRACT LDA VALUE_O  # Loads the contents of the memory location of the label: VALUE_O to the accumulator
+         SUB VALUE_T  # Subtract the contents of memory address of the label: NUM to the accumulator
+         BRA END      # Branches to the memory location of the label: END
 
-END      OUT
-         HLT
+END      OUT          # Outputs the contents of the accumulator
+         HLT          # Signify the end of the program
 
-MODE     DAT 1
-VALUE_O  DAT 1
-VALUE_T  DAT 2
+MODE     DAT 1        # Defines the label: MODE as containing data, and sets it to a preset of 1
+VALUE_O  DAT 1        # Defines the label: VALUE_O as containing data, and sets it to a preset of 1
+VALUE_T  DAT 2        # Defines the label: VALUE_T as containing data, and sets it to a preset of 2
